caef4d6820e7ab96f6c2052b1630c1c8f12f8ec1
Aaron Thompson <thompsonaaron@me.com>
2017-12-14 18:38:50 -0800

    Initial Commit

---
+++ WordTransformation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WordTransformation
+//
+//  Created by Aaron Thompson on 12/14/17.
+//  Copyright © 2017 Aaron Thompson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
